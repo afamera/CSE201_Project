ec48fb8b1aec02409fc05f61e9de2f95ae154003
B-Tian1996 <49376028+B-Tian1996@users.noreply.github.com>
2019-10-27 03:15:45 -0400

    uodata code for web application

---
+++ RazorPagesShowSearcher/RazorPagesShowSearcher/Models/Show.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesShowSearcher.Models
+{
+    public class Show
+    {
+      
+
+        public int ID { get; set; }
+        public string ShowName { get; set; }
+        public string ShowDescription { get; set; }
+        public double ShowRate { get; set; }
+        public int NumOfSeasons { get; set; }
+        public int YearShowBegan { get; set; }
+        public string Genra { get; set; }
+        public string ShowTitle { get; set; }
+        public string OriginalNetwork { get; set; }
+
+ 
+
+        
+    }
+}
---
+++ RazorPagesShowSearcher/RazorPagesShowSearcher/Pages/Shows/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesShowSearcher.Data;
+using RazorPagesShowSearcher.Models;
+
+namespace RazorPagesShowSearcher.Pages.Shows
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesShowSearcher.Data.RazorPagesShowSearcherContext _context;
+
+        public IndexModel(RazorPagesShowSearcher.Data.RazorPagesShowSearcherContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Show> Show { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Show = await _context.Show.ToListAsync();
+        }
+    }
+}
