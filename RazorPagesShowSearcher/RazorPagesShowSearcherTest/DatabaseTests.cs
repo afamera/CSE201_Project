e440577afdcd7af2f05f013669d8d90dbc4469c8
yuqinbs <54953396+yuqinbs@users.noreply.github.com>
2019-10-29 16:36:08 -0400

    Test whether the database of Show works
    union test

---
+++ RazorPagesShowSearcher/RazorPagesShowSearcherTest/DatabaseTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RazorPagesShowSearcherTest
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        [TestMethod]
+        public void TestDatabaseFileCreate()
+        {
+            File.Delete("database.db");
+            var db = new DatabaseTests();
+            db.Load();
+            Assert.IsTrue(File.Exists("database.db"));
+            File.Delete("database.db");
+        }
+        [TestMethod]
+        public void TestAddRecord()
+        {
+            File.Delete("database.db");
+            var db = new Database();
+            db.Load();
+            var s = new Show
+            {
+                Country = "country1",
+                Id = "1",
+                Plot = "plot1",
+                Poster = "poster1",
+                Showtime = "110 min",
+                Success = true,
+                Title = "title1",
+                Year = "2012"
+            };
+            db.AddToFavorites(s);
+            db.Dispose();
+
+            var db2 = new Database();
+            db2.Load();
+            var favs = db2.GetAllFavorites().ToArray();
+            db2.Dispose();
+            Assert.AreEqual(favs.Length, 1);
+            Assert.AreEqual(favs[0].Title, s.Title);
+            Assert.AreEqual(favs[0].Country, s.Country);
+            Assert.AreEqual(favs[0].Id, s.Id);
+            Assert.AreEqual(favs[0].Plot, s.Plot);
+            Assert.AreEqual(favs[0].Poster, s.Poster);
+            Assert.AreEqual(favs[0].Runtime, s.Showtime);
+            Assert.AreEqual(favs[0].Success, s.Success);
+            Assert.AreEqual(favs[0].Year, s.Year);
+            File.Delete("database.db");
+        }
+
+        [TestMethod]
+        public void TestDeleteRecord()
+        {
+            File.Delete("database.db");
+            var db = new Database();
+            db.Load();
+            var s = new Show
+            {
+                Country = "country1",
+                Id = "1",
+                Plot = "plot1",
+                Poster = "poster1",
+                Showtime = "110 min",
+                Success = true,
+                Title = "title1",
+                Year = "2012"
+            };
+            db.AddToFavorites(s);
+            db.Dispose();
+
+            var db2 = new Database();
+            db2.Load();
+            db2.RemoveFromFavorites(s.Id);
+            db2.Dispose();
+
+            var db3 = new Database();
+            db3.Load();
+            var favs = db3.GetAllFavorites().ToArray();
+            db3.Dispose();
+            Assert.AreEqual(favs.Length, 0);
+            File.Delete("database.db");
+        }
+    }
+}
---
+++ RazorPagesShowSearcher/RazorPagesShowSearcherTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RazorPagesShowSearcherTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RazorPagesShowSearcherTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14eaf5de-4635-4948-b237-d613e9f57237")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
