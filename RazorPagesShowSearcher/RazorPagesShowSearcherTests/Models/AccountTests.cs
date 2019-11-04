8681264e40197171ee21659c65f51a13de7799f8
yuqinbs <54953396+yuqinbs@users.noreply.github.com>
2019-11-04 14:54:24 -0500

    unit test

---
+++ RazorPagesShowSearcher/RazorPagesShowSearcherTests/Models/AccountTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RazorPagesShowSearcher.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RazorPagesShowSearcher.Models.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void addToFavoritesTest()
+        {
+            Assert.True();
+        }
+       
+
+        [TestMethod()]
+        public void removeFromFavoritesTest()
+        {
+            Assert.True();
+        }
+
+        [TestMethod()]
+        public void likeShowTest()
+        {
+            Assert.True();
+        }
+
+        [TestMethod()]
+        public void dislikeShowTest()
+        {
+            Assert.True();
+        }
+
+        [TestMethod()]
+        public void addShowInfoTest()
+        {
+            Assert.True();
+        }
+
+        [TestMethod()]
+        public void adeleteShowInfoTest()
+        {
+            Assert.True();
+        }
+
+        [TestMethod()]
+        public void editShowInfoTest()
+        {
+            Assert.True();
+        }
+    }
+}
