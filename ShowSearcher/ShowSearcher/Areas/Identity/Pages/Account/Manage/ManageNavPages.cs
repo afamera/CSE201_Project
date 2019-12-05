8c41a9d063373d1e266881b37d56daccb7416fc1
Tianyiyang111 <39191699+Tianyiyang111@users.noreply.github.com>
2019-12-04 22:55:03 -0500

    123456

--- ShowSearcher/ShowSearcher/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ ShowSearcher/ShowSearcher/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -18,7 +18,6 @@ namespace ShowSearcher.Areas.Identity.Pages.Account.Manage
 
         public static string PersonalData => "PersonalData";
 
-        public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
 
@@ -30,7 +29,7 @@ namespace ShowSearcher.Areas.Identity.Pages.Account.Manage
 
         public static string PersonalDataNavClass(ViewContext viewContext) => PageNavClass(viewContext, PersonalData);
 
-        public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
+       
 
         private static string PageNavClass(ViewContext viewContext, string page)
         {
